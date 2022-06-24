1e71b2b726313fd16678ee780e495367b9cc0013
AreaEffectCloud <84448503+AreaEffectCloud@users.noreply.github.com>
2022-06-24 12:01:12 +0900

    history

---
+++ Calculator_beta/History_form.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Calculator_beta
+{
+    partial class History_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(History_form));
+            this.history = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // history
+            // 
+            this.history.Font = new System.Drawing.Font("游明朝", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.history.FormattingEnabled = true;
+            this.history.ItemHeight = 27;
+            this.history.Items.AddRange(new object[] {
+            "履歴"});
+            this.history.Location = new System.Drawing.Point(-2, 0);
+            this.history.Name = "history";
+            this.history.Size = new System.Drawing.Size(418, 544);
+            this.history.TabIndex = 0;
+            // 
+            // History_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 554);
+            this.Controls.Add(this.history);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "History_form";
+            this.Text = "History_form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox history;
+    }
+}
---
+++ Calculator_beta/History_form.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_beta
+{
+    public partial class History_form : Form
+    {
+        public History_form()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            //最大サイズ、最小サイズを固定
+            this.MaximumSize = this.Size;
+            this.MinimumSize = this.Size;
+            //フォームが最大化されないようにする
+            this.MaximizeBox = false;
+        }
+
+        /*
+         * ここでインスタンスを生成することで、これを表示する際に、複数のウィンドウが作られなくなる
+         * Show() は Function_Calculator で実行
+         */
+
+        private static History_form hf_instance;
+        public static History_form Instance
+        {
+            get
+            {
+                //_instanceがnullまたは破棄されているときは、
+                //新しくインスタンスを作成する
+                if (hf_instance == null || hf_instance.IsDisposed)
+                {
+                    hf_instance = new History_form();
+                }
+                return hf_instance;
+            }
+        }
+    }
+}
